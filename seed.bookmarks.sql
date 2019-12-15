b106664f55e80b84b3c3703a100e356e2760fe87
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2019-12-15 13:56:58 -0800

    initial commit: practicing expess and databases; made a seed and create script for use with a postgresql database

---
+++ seed.bookmarks.sql
@@ -0,0 +1,7 @@
+insert into bookmarks (title, url, description)
+values 
+('google', 'https://google.com', 'literally the whole internet except the the dark net'),
+('reddit', 'https://reddit.com', 'the front page of the internet or so I''m told'),
+('thinkful', 'https://overview.thinkful.com', 'a place to learn'),
+('notcot', 'https://notcot.com', 'a place to get inspired'),
+('geekhack', 'https://geekhack.org', 'typin coinoisseurs');
